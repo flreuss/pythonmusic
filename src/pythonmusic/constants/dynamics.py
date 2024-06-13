844e1340227838f102a7a5ce4cb9e5532570691f
Darius Löschner <darius.loeschner@uni-potsdam.de>
2024-06-13 16:16:59 +0200

    defines constants for note dynamics

---
+++ src/pythonmusic/constants/dynamics.py
@@ -0,0 +1,18 @@
+FFF: int = 127
+"""Defines dynamic constant for *forte fortissimo*."""
+FF: int = 111
+"""Defines dynamic constant for *fortissimo*."""
+F: int = 94
+"""Defines dynamic constant for *forte*."""
+MF: int = 79
+"""Defines dynamic constant for *mezzoforte*."""
+MP: int = 63
+"""Defines dynamic constant for *mezzopiano*."""
+P: int = 47
+"""Defines dynamic constant for *piano*."""
+PP: int = 32
+"""Defines dynamic constant for *pianissimo*."""
+PPP: int = 16
+"""Defines dynamic constant for *piano pianissimo*."""
+MUTE: int = 0
+"""Defines dynamic constant for muted notes."""
