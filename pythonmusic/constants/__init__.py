86ddafef977908f966a63dc27b47159336eb2625
Darius Löschner <darius.loeschner@uni-potsdam.de>
2024-06-26 16:15:18 +0200

    fixed invalid import paths in constant module init

--- pythonmusic/constants/__init__.py
+++ pythonmusic/constants/__init__.py
@@ -2,11 +2,11 @@
 This module defines several MIDI constants.
 """
 
-from durations import *
-from dynamics import *
-from instruments import *
-from panning import *
-from percussion import *
-from pitches import *
-from scales import *
-from tempo import *
+from .durations import *
+from .dynamics import *
+from .instruments import *
+from .panning import *
+from .percussion import *
+from .pitches import *
+from .scales import *
+from .tempo import *
