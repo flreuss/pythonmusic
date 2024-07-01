9636270994392b7d8c1f74b23ada1161ee9e5981
Darius Löschner <darius.loeschner@uni-potsdam.de>
2024-07-01 20:52:33 +0200

    added star import to __init__.py in main module

--- pythonmusic/__init__.py
+++ pythonmusic/__init__.py
@@ -3,3 +3,10 @@
 # TODO: add custom errors
 # TODO: replace map(filter) with reduce
 # TODO: add annotations to class properties
+
+from .constants import *
+from .io import *
+from .music import *
+from .osc import *
+from .play import *
+from .util import *
