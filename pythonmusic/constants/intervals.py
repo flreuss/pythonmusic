fc9eba8b25c1a8596493af103f25bade793100e6
Darius Löschner <darius.loeschner@uni-potsdam.de>
2024-07-07 16:28:28 +0200

    added interval constants

---
+++ pythonmusic/constants/intervals.py
@@ -0,0 +1,30 @@
+"""
+Defines pitch interval offsets.
+"""
+
+UNISON: int = 0
+"""Interval constant for a perfect unison."""
+MINOR_SECOND: int = 1
+"""Interval constant for a minor second."""
+MAJOR_SECOND: int = 2
+"""Interval constant for a major second."""
+MINOR_THIRD: int = 3
+"""Interval constant for a minor third."""
+MAJOR_THIRD: int = 4
+"""Interval constant for a major third."""
+FOURTH: int = 5
+"""Interval constant for a perfect fourth."""
+TRITONE: int = 6
+"""Interval constant for a tritone, augmented fourth, or diminished fifth."""
+FIFTH: int = 7
+"""Interval constant for a perfect fifth."""
+MINOR_SIXTH: int = 8
+"""Interval constant for a minor sixth."""
+MAJOR_SIXTH: int = 9
+"""Interval constant for a major sixth."""
+MINOR_SEVENTH: int = 10
+"""Interval constant for a minor seventh."""
+MAJOR_SEVENTH: int = 11
+"""Interval constant for a major seventh."""
+OCTAVE: int = 12
+"""Interval constant for a perfect octave."""
