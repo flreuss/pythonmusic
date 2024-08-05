cec7b927a879db7f51052573ee01465754de7798
Darius Löschner <darius.loeschner@uni-potsdam.de>
2024-08-05 18:11:08 +0200

    added explination for midi port names

--- pythonmusic/io/__init__.py
+++ pythonmusic/io/__init__.py
@@ -11,6 +11,12 @@ from mido import get_output_names as _get_output_names  # type: ignore [reportAt
 def get_midi_receivers() -> list[str]:
     """
     Returns a list of available midi ports that listen for messages.
+
+    Depending on your platform, your system may name midi ports differently.
+    Even if you pass a specific name to MidiReceiver, your system may end up
+    using a slightly different name. For instance, "ExampleMidiReceiver" may show
+    up as "RtMidiIn Client:ExampleMidiReceiver 128:0". Use this function to
+    retrieve the valid name of your receivers etc.
     """
     return _cast(list[str], _get_output_names())
 
@@ -18,5 +24,11 @@ def get_midi_receivers() -> list[str]:
 def get_midi_senders() -> list[str]:
     """
     Returns a list of available midi ports that send midi messages.
+
+    Depending on your platform, your system may name midi ports differently.
+    Even if you pass a specific name to MidiSender, your system may end up
+    using a slightly different name. For instance, "ExampleMidiSender" may show
+    up as "RtMidiOut Client:ExampleMidiSender 128:0". Use this function to
+    retrieve the valid name of your senders etc.
     """
     return _cast(list[str], _get_input_names())
