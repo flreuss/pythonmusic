6e43e4fdcc6cb84a371e9dd6d4840a4e53a48c3a
Darius Löschner <darius.loeschner@uni-potsdam.de>
2024-08-10 16:13:08 +0200

    renamed scale constants to avoid collision with chords

--- pythonmusic/constants/scales.py
+++ pythonmusic/constants/scales.py
@@ -2,52 +2,52 @@
 Defines common scales by their offset from a root note.
 """
 
-MAJOR: list[int] = [0, 2, 4, 5, 7, 9, 11]
+MAJOR_SCALE: list[int] = [0, 2, 4, 5, 7, 9, 11]
 """Scale constant for the major scale."""
 
-MINOR: list[int] = [0, 2, 3, 5, 7, 8, 10]
+MINOR_SCALE: list[int] = [0, 2, 3, 5, 7, 8, 10]
 """Scale constant for the (natrual) minor scale."""
 
-HARMONIC_MINOR: list[int] = [0, 2, 3, 5, 7, 8, 11]
+HARMONIC_MINOR_SCALE: list[int] = [0, 2, 3, 5, 7, 8, 11]
 """Scale constant for the harmonic minor scale."""
 
-MELODIC_MINOR: list[int] = [0, 2, 3, 5, 7, 9, 11]
+MELODIC_MINOR_SCALE: list[int] = [0, 2, 3, 5, 7, 9, 11]
 """Scale constant for the melodic minor scale."""
 
-CHROMATIC: list[int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+CHROMATIC_SCALE: list[int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 """SCale constant for the chromatic scale."""
 
-WHOLE_TONE: list[int] = [0, 2, 4, 6, 8, 10]
+WHOLE_TONE_SCALE: list[int] = [0, 2, 4, 6, 8, 10]
 """Scale constant for the whole tone scale."""
 
-PENTATONIC: list[int] = [0, 2, 4, 7, 9]
+PENTATONIC_SCALE: list[int] = [0, 2, 4, 7, 9]
 """Scale constant for the pentatonic scale."""
 
-IONIAN: list[int] = MAJOR
+IONIAN_SCALE: list[int] = MAJOR_SCALE
 """
 Scale constants for the ionian scale.
 
 This is identical to the major scale.
 """
 
-DORIAN: list[int] = [0, 2, 3, 5, 7, 9, 10]
+DORIAN_SCALE: list[int] = [0, 2, 3, 5, 7, 9, 10]
 """Scale constant for the dorian or doric scale."""
 
-PHRYGIAN: list[int] = [0, 1, 3, 5, 7, 8, 10]
+PHRYGIAN_SCALE: list[int] = [0, 1, 3, 5, 7, 8, 10]
 """Scale constant for the phrygian scale."""
 
-LYDIAN: list[int] = [0, 2, 4, 6, 7, 9, 11]
+LYDIAN_SCALE: list[int] = [0, 2, 4, 6, 7, 9, 11]
 """Scale constant for the lydian scale."""
 
-MIXOLYDIAN: list[int] = [0, 2, 4, 5, 7, 9, 10]
+MIXOLYDIAN_SCALE: list[int] = [0, 2, 4, 5, 7, 9, 10]
 """Scale constant for the mixolydian scale."""
 
-AEOLIAN: list[int] = MINOR
+AEOLIAN_SCALE: list[int] = MINOR_SCALE
 """
 Scale constant for the aeolian scale.
 
 This is identical to the minor scale.
 """
 
-LOCRIAN: list[int] = [0, 1, 2, 3, 4, 6, 8, 10]
+LOCRIAN_SCALE: list[int] = [0, 1, 2, 3, 4, 6, 8, 10]
 """Scale constant for the locrian scale."""
