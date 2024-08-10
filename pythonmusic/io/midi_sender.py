3a5c71d5f56609322e6b1bc8439af0053f4da009
MaggieLostKeys <61566179+MaggieLostKeys@users.noreply.github.com>
2024-08-10 14:15:15 +0200

    added checks for port safety

--- pythonmusic/io/midi_sender.py
+++ pythonmusic/io/midi_sender.py
@@ -1,8 +1,7 @@
-from typing import Self, cast as _cast
+from typing import Self
 
-from mido.messages import Message as _Midomessage
 from mido import open_output as _open_output  # type: ignore [reportAttributeAccessIssue]
-from mido.backends.rtmidi import Output as _Output
+from mido.backends.rtmidi import Output as RtOutput
 
 from .midi_message import MidiMessage
 
@@ -14,10 +13,25 @@ class MidiSender:
 
     def __init__(self, name: str) -> None:
         self.name: str | None = name
-        self.port: _Output = _open_output(name, virtual=True)
+        self._port: RtOutput = _open_output(name, virtual=True)
 
     def __del__(self):
         self.port.close()
+
+    def _assert_open_port(self):
+        """
+        Panics if this sender's port is not open.
+
+        The `port` property is public. Closing the output manually will trigger
+        a segfault if it's accessed later. This method should prevent that.
+        """
+        if self._port.closed:
+            raise IOError(f'Port for "{self.name}" is already closed')
+
+    @property
+    def port(self) -> RtOutput:
+        self._assert_open_port()
+        return self._port
 
     @classmethod
     def attach(cls, output_name: str) -> Self:
@@ -33,7 +47,7 @@ class MidiSender:
 
         new = cls.__new__(cls)
         new.name = None
-        new.port = port
+        new._port = port
 
         return new
 
@@ -42,3 +56,19 @@ class MidiSender:
         Sends the given midi message to this sender's output.
         """
         self.port.send(msg.raw())
+
+    def reset(self):
+        """
+        Sends note off and reset controller on all channels.
+
+        Ideally, this method resets the attached midi receiver to its default
+        state.
+        """
+        self.port.reset()
+
+    def force_note_off(self):
+        """
+        Instructs the attached receiver to abort all notes. This does not reset
+        controller values.
+        """
+        self.port.panic()
