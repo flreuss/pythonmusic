3a3da3154125f5e68173f088cdf616ba2e7d3491
Darius Löschner <darius.loeschner@uni-potsdam.de>
2024-09-03 14:49:56 +0200

    import Synth and SynthPlayer only if FluidSynth is available

--- pythonmusic/__init__.py
+++ pythonmusic/__init__.py
@@ -4,5 +4,12 @@ from .io import *
 from .music import *
 from .osc import *
 from .play import *
-from .synth import *
 from .util import *
+
+try:
+    from .synth import *
+except ImportError as e:
+    if str(e) == "Couldn't find the FluidSynth library.":
+        pass
+    else:
+        raise e
--- pythonmusic/play/__init__.py
+++ pythonmusic/play/__init__.py
@@ -1,4 +1,1 @@
 from .midi_player import *
-from .synth_player import *
-
-# from .player import *
