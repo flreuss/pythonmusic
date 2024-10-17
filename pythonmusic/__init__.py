74cf9cfa5dfec7202742a6535803b07494873a33
Darius Löschner <darius.loeschner@uni-potsdam.de>
2024-10-17 15:37:51 +0200

    moved code player to play module

--- pythonmusic/__init__.py
+++ pythonmusic/__init__.py
@@ -4,7 +4,6 @@ from .io import *
 from .music import *
 from .play import *
 from .util import *
-from .code_play import *
 
 # This ensures that the fluid synth parts of this library are only imported if
 #  fluidsynth is installed. The binding library will check and returns the error
