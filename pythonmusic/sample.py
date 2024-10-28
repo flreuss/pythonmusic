b641485e77a8be0c52a276541d448565527554fc
Darius Löschner <darius.loeschner@uni-potsdam.de>
2024-10-28 16:43:18 +0100

    progress on sampler

--- pythonmusic/sample.py
+++ pythonmusic/sample.py
@@ -1,10 +1,17 @@
 from abc import abstractmethod, ABC
 from pathlib import Path
-from typing import Mapping, Optional
+from typing import Mapping, Optional, Union, cast
 from time import sleep
+import numpy as np
 
 from pyaudio import PyAudio, Stream, paContinue
 import wave
+
+from pythonmusic.constants.dynamics import MF
+from pythonmusic.util import assert_range
+
+# TODO: look into [pydub](https://github.com/jiaaro/pydub?tab=readme-ov-file#installation)
+# not really maintained and undocumented, but easier to work with
 
 
 __all__ = ["AudioSample", "AudioSampler"]
@@ -46,6 +53,18 @@ class _Sample(ABC):
         self.stream().stop_stream()
         self.reset()
 
+    @abstractmethod
+    def volume(self) -> int: ...
+
+    @abstractmethod
+    def set_volume(self, value: int): ...
+
+    @abstractmethod
+    def set_falloff(self, value: float): ...
+
+    @abstractmethod
+    def get_falloff(self) -> float: ...
+
 
 class _WaveSample(_Sample):
     def __init__(
@@ -62,6 +81,9 @@ class _WaveSample(_Sample):
             output=True,
             stream_callback=self.callback,
         )
+        self.bits = self.wave_file.getsampwidth() * 8
+        self.multiplyer = 1.0
+        self.falloff = -1.0
 
         # prevent stream from starting immediately
         self._stream.stop_stream()
@@ -81,11 +103,49 @@ class _WaveSample(_Sample):
         time_info: Mapping[str, float],
         status: int,
     ) -> tuple[bytes, int]:
+        # read data
         data = self.wave_file.readframes(frame_count)
+
+        # determine array type (may do damage idk)
+        conv_type = np.int16 if self.bits == 16 else np.int8
+
+        # convert to np array
+        np_array = np.frombuffer(data, conv_type)
+
+        # multiply to change volume
+        multiplyer = self.multiplyer
+        if self.falloff > 0.0:
+            fr = float(self.wave_file.getframerate())
+            t = float(CHUNK_SIZE) / fr
+            step = t / self.multiplyer
+            self.falloff -= t
+
+            multiplyer *= step
+            multiplyer = min(1, max(0, multiplyer))
+        np_array = np_array * multiplyer
+
+        # clip range to avoid over-/underflow
+        np_array.clip(-32768, 32767)
+
+        # convert back to bytes object
+        data = np_array.astype(np.int16).tobytes()
+
         return data, paContinue
 
     def reset(self):
         self.wave_file.rewind()
+
+    def volume(self) -> int:
+        return round(self.multiplyer * 127.0)
+
+    def set_volume(self, value: int):
+        self.multiplyer = float(value) / 127.0
+
+    def set_falloff(self, value: float):
+        self.falloff = value
+
+    def get_falloff(self) -> float:
+        return self.falloff
 
 
 # An individual sound file that is loaded and stored inside the audio sampler
@@ -102,6 +162,7 @@ class AudioSample:
         self._port_audio = PyAudio()
         self._sample: _Sample = self._load_audio_file()
         self._is_playing: bool = False
+        self._volume: int = MF
 
     def __del__(self):
         # stream closes on del of sample
@@ -125,6 +186,9 @@ class AudioSample:
         )
 
     def is_playing(self) -> bool:
+        """
+        Returns `True` if the player is currently playing.
+        """
         return self._sample.is_playing()
 
     def play(self, block: bool = True):
@@ -185,6 +249,41 @@ class AudioSample:
         self.pause()
         self._sample.stop()
 
+    def volume(self) -> int:
+        """
+        Returns the sample's volume in range from 0 to 127.
+
+        Returns:
+            int: The volume of the sample
+        """
+        return self._sample.volume()
+
+    def set_volume(self, value: int):
+        """
+        Sets the sample's volume.
+
+        .. important:: The given value must be in range from 0 to 127.
+
+        Args:
+            value(int): An int in range from 0 to 127
+        """
+        assert_range(value, 0, 127)
+        self._sample.set_volume(value)
+
+    def set_falloff(self, time: float):
+        """
+        Phases out the currently playing sound.
+
+        Use this to avoid an audible cut-off when stopping notes.
+        """
+        self._sample.set_falloff(time)
+
+    def get_falloff(self) -> float:
+        """
+        Returns the remaining falloff.
+        """
+        return self._sample.get_falloff()
+
 
 # A sampler that stores audio samples and plays them back as required by
 # a note's pitch, duration, etc.
@@ -193,30 +292,77 @@ class AudioSampler:
     # a new PyAudio instance can be created (and terminated) for each audio
     # samler (docs, I guess)
     def __init__(self):
-        self._pyaudio = PyAudio()
-        # self._stream =
+        self._samples: list[Optional[AudioSample]] = [None] * 128
 
-    def __del__(self):
-        self._pyaudio.terminate()
+    def add_sample(self, sample: AudioSample, notes: Union[int, list[int]]):
+        """
+        Registers a sample to a note pitch value or values.
 
-    #     self.stream.stop_stream()
-    #     self.stream.close()
+        The given note pitches must be in range from 0 to 127 (included).
+
+        Args:
+            sample (AudioSample): An audio sample
+            notes (int | list[int]): Either an int which represents the pitch
+                value to add the sample to, or a list of ints, in which case, the
+                sample will be added to all pitches
+        """
+        if type(notes) == int:
+            self._samples[notes] = sample
+        else:  # crash if wrong error
+            notes = cast(list[int], notes)
+            for note in notes:
+                self._samples[note] = sample
+
+    def clear_sample(self, note: int) -> Optional[AudioSample]:
+        """
+        Removes the audio sample from the given note pitch and returns it.
+
+        Args:
+            note (int): The note pitch to remove the sample from
+
+        Returns:
+            Optional[AudioSample]: The removed audio sample, or `None` if no
+                sample was registered for the given pitch
+        """
+        sample = self._samples[note]
+        self._samples[note] = None
+        return sample
+
+    def has_sample(self, note: int) -> bool:
+        """
+        Returns `True` if a sample for the given note pitch has been registered.
+
+        Args:
+            note (int): A note pitch
+
+        Returns:
+            bool: `True` if a sample has been registered for the given pitch
+        """
+        return self._samples[note] is not None
+
+    def get_sample(self, note: int) -> Optional[AudioSample]:
+        """
+        Returns the registered AudioSample for the given pitch, if any.
+
+        Args:
+            note (int): A note pitch
+
+        Returns:
+            Optional[AudioSample]: The audio sample for the given pitch, or
+                `None`, if none has been registered
+        """
+        return self._samples[note]
 
     # ==== EVENTS ====
-    def note_on(self, channel: int, note: int, velocity: int):
-        raise NotImplemented()
+    def note_on(self, note: int, velocity: int):
+        sample = self._samples[note]
+        if sample is not None:
+            sample.stop()
+            sample.set_falloff(-1.0)
+            sample.set_volume(velocity)
+            sample.play(block=False)
 
-    def note_off(self, channel: int, note: int):
-        raise NotImplemented()
-
-    def set_cc(self, channel: int, control: int, value: int):
-        raise NotImplemented()
-
-    def cc(self, channel: int, control: int) -> int:
-        raise NotImplemented()
-
-    def select_patch(self, channel: int, patch: int):
-        raise NotImplemented()
-
-    def patch(self, channel: int) -> int:
-        raise NotImplemented()
+    def note_off(self, note: int):
+        sample = self._samples[note]
+        if sample is not None:
+            sample.set_falloff(0.5)
