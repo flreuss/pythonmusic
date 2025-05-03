4aa8553a7b66ebf10ac037bd233d0126a97a0784
Darius Löschner <darius.loeschner@uni-potsdam.de>
2025-05-03 14:24:46 +0200

    osc

--- pythonmusic/osc.py
+++ pythonmusic/osc.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pythonosc import osc_server, udp_client
 from pythonosc.dispatcher import Dispatcher
 
@@ -10,9 +12,9 @@ class OscIn:
         self._ip = ip
         self._port = port
         self._dispatcher = Dispatcher()
-        self._server = osc_server.ThreadingOSCUDPServer(
-            (self._ip, self._port), self._dispatcher
-        )
+        self._server: osc_server.ThreadingOSCUDPServer
+
+        self.map = self._dispatcher.map
 
     def ip(self) -> str:
         """
@@ -26,11 +28,45 @@ class OscIn:
         """
         return self._port
 
-    # def map(self, address: str, callback, ):
-    #     self._dispatcher.map
+    def server(self) -> Optional[osc_server.ThreadingOSCUDPServer]:
+        """
+        Returns the internal osc server.
+        """
+        return self._server
+
+    def start(self):
+        """
+        Starts listening to incoming osc messages.
+        """
+        self._server = osc_server.ThreadingOSCUDPServer(
+            (self._ip, self._port), self._dispatcher
+        )
+        self._server.serve_forever()
 
 
 # (client)
 class OscOut:
-    def __init__(self):
-        pass
+    def __init__(self, ip: str = "127.0.0.1", port: int = 5005):
+        self._client = udp_client.SimpleUDPClient(ip, port)
+        self._ip = ip
+        self._port = port
+
+        self.send_message = self._client.send_message
+
+    def ip(self) -> str:
+        """
+        Returns the client's ip.
+        """
+        return self._ip
+
+    def port(self) -> int:
+        """
+        Returns the client's port.
+        """
+        return self._port
+
+    def client(self) -> udp_client.SimpleUDPClient:
+        """
+        Returns the internal client.
+        """
+        return self._client
